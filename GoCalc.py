618b6a605fb2a0de0ac0e4f413107a4e09d83244
Keeper <hd_keeper@mail.ru>
2015-01-19 15:22:10 +0300

    Session undo/redo

--- GoCalc.py
+++ GoCalc.py
@@ -2,6 +2,7 @@
 
 from exceptions import IndexError
 from random import randint
+import pickle
 
 BLACK = 1
 WHITE = 2
@@ -56,8 +57,8 @@ class Board (object):
             self.prevBoard.nextBoards.append(self)
             self.size = self.prevBoard.size
             self.turn = self.prevBoard.turn + 1
-            for (pos, place) in self.prevBoard.places.iteritems():
-                self.places[pos] = Place( place.color, place.turn)
+            for (xy, place) in self.prevBoard.places.iteritems():
+                self.places[xy] = Place( place.color, place.turn)
 
     def __str__(self):
         return ''.join(
@@ -72,18 +73,18 @@ class Board (object):
             raise Exception('Offset %d, %d is already occupied' % (x, y))
 
         newBoard = Board(board=self)
-        newBoard._putStone(x, y, color)
+        newBoard._putStone( (x, y), color)
         return newBoard
 
-    def _putStone(self, x, y, color):
-        self.places[(x,y)] = Place(color, self.turn)
-        self.lastMove = (x, y)
+    def _putStone(self, xy, color):
+        self.places[xy] = Place(color, self.turn)
+        self.lastMove = xy
         self._findGroups()
         self._countDame()
         self._removeDeadGroups()
 
-    def _getAdjacentPositions(self, pos):
-        x, y = pos
+    def _getAdjacentPositions(self, xy):
+        x, y = xy
         if x > 1:
             yield (x-1, y)
         if y > 1:
@@ -94,18 +95,18 @@ class Board (object):
             yield (x, y+1)
         
     def _findGroups(self):
-        def addPosToGroup(pos, group):
-            group.stoneCoords.add(pos)
+        def addPosToGroup(xy, group):
+            group.stoneCoords.add(xy)
             group.stoneCount += 1
-            self.places[pos].group = group
-            for p in self._getAdjacentPositions(pos):
+            self.places[xy].group = group
+            for p in self._getAdjacentPositions(xy):
                 if self.places[p].color == group.color and self.places[p].group is None:
                     addPosToGroup( p, group)
 
-        for (pos, place) in self.places.iteritems():
+        for (xy, place) in self.places.iteritems():
             if place.group is None and place.color is not None:
                 newGroup = Group(place.color)
-                addPosToGroup(pos, newGroup)
+                addPosToGroup(xy, newGroup)
                 self.groups.append(newGroup)
     
     def _countDame(self):
@@ -121,8 +122,8 @@ class Board (object):
         recount = False
         for group in self.groups:
             if group.dameCount == 0:
-                for pos in group.stoneCoords:
-                    self.places[pos] = Board.emptyPlace
+                for xy in group.stoneCoords:
+                    self.places[xy] = Board.emptyPlace
                 recount = True
 
         self.groups = [g for g in self.groups if g.dameCount > 0]
@@ -130,17 +131,62 @@ class Board (object):
             self._countDame()
 
                         
+class Session (object):
 
+    def __init__(self, size):
+        self.first = self.current = self.last = Board(size=size)
 
+    def save(self, filename):
+        with open(filename, 'w') as f:
+            pickle.dump(self, f)
 
+    @staticmethod
+    def load(filename):
+        return pickle.load(open(filename))
 
+    def makeMove(self, x, y, color):
+        self.current = self.last = self.current.makeMove( x, y, color)
+    
+    def undo(self):
+        if self.current.prevBoard is None:
+            return False
+        else:
+            self.current = self.current.prevBoard
+            return True
+
+    def redo(self, xy=None):
+        if len(self.current.nextBoards) == 0:
+            return False
+        elif len(self.current.nextBoards) == 1:
+            self.current = self.current.nextBoards[0]
+            return True
+        else:
+            for next in self.current.nextBoards:
+                if next.lastMove == xy:
+                    self.current = next
+                    return True
+            return False
+        
 
 
 if __name__ == '__main__':
-    first = Board( size=9)
-    last = first.makeMove(1, 1, BLACK).makeMove(1, 2, WHITE).makeMove(2, 2, WHITE).makeMove(2, 1, WHITE)
 
-    print last
+    def testSave():
+        s = Session(9)
+        s.makeMove(1, 1, BLACK)
+        s.makeMove(1, 2, WHITE)
+        s.makeMove(2, 2, WHITE)
+        s.makeMove(2, 1, WHITE)
+        print s.current
+        s.save('session.save')
+
+    def testLoad():
+        s = Session.load('session.save')
+        print s.current
+        s.undo()
+        print s.current
+
+    testLoad()
 
 
 
